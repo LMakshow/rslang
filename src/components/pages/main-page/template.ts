eb228282b475bd5346170bd6b9a7e1baee043380
Bella Makhova <rgvder@gmail.com>
2022-08-17 16:40:03 +0300

    feat: add responsive to main page layout

--- src/components/pages/main-page/template.ts
+++ src/components/pages/main-page/template.ts
@@ -1,24 +1,26 @@
 const templateMain: string = `
   <div class="main__wrapper wrapper">
-    <div class="main__content">
-       <h1 class="main__heading heading">
-       Учи английский просто и с умом!
-       </h1>
-       <ul class="main__items">
-        <li class="main__item">3600 слов для любого уровня</li>
-        <li class="main__item">Мини-игры для запоминания</li>
-        <li class="main__item">Статистика в доступной форме</li>
-      </ul>
-      <div class="main__buttons">
-      <a class="button button-additional button-start" href="#">Начать!</a>
-      <a class="button button-basic button-team" href="#">О команде</a>
+    <div class="main__container">
+      <div class="main__content">
+        <h1 class="main__heading heading">
+          Учи английский просто и с умом!
+        </h1>
+        <ul class="main__items">
+          <li class="main__item">3600 слов для любого уровня</li>
+          <li class="main__item">Мини-игры для запоминания</li>
+          <li class="main__item">Статистика в доступной форме</li>
+        </ul>
+        <div class="main__buttons">
+          <a class="button button-additional button-start" href="#">Начать!</a>
+          <a class="button button-basic button-team" href="#">О команде</a>
+        </div>
       </div>
-    </div>
-    <div class="main__image">
-      <div class="speech">
-        <p class="speech__text">Я Рислинг! Учи английский вместе со мной!</p>
-    </div>
-    <img class="main__dog" src="assets/images/main-page/main-dog.svg" alt="Risling">
+      <div class="main__image">
+        <div class="speech">
+          <p class="speech__text">Я Рислинг! Учи английский вместе со мной!</p>
+        </div>
+        <img class="main__dog" src="assets/images/main-page/main-dog.svg" alt="Risling">
+      </div>
     </div>
   </div>`;
 
