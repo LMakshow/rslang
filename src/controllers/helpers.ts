f68facd4002f3d8a33a813d45184e20a058515a3
Maksym Lytvyn <makshow.at@gmail.com>
2022-08-19 17:17:19 +0300

    feat: use renderElement for multiple classes

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,13 +1,24 @@
+/**
+ * Creates some element with a template and classes and appends it to the parent container
+ * @param tag - tag of the element to create
+ * @param templateElement - innerHTML of the element to create
+ * @param container - parent of the element
+ * @param classStyle - add a style (string) or multiple styles (string array)
+ */
 const renderElement = (
   tag: string,
   templateElement: string,
   container: HTMLElement,
-  classStyle?: string,
+  classStyle?: string | string[],
 ) => {
   const element: HTMLElement = document.createElement(tag) as HTMLElement;
 
-  if (classStyle) {
+  if (typeof classStyle === 'string') {
     element.classList.add(classStyle);
+  }
+
+  if (Array.isArray(classStyle)) {
+    classStyle.forEach((style) => element.classList.add(style));
   }
 
   element.innerHTML = templateElement;
