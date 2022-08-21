d02a3cd11fa91a377e01b259702c96343f8737e3
Bella Makhova <rgvder@gmail.com>
2022-08-21 22:17:54 +0300

    feat: add entrance popup

---
+++ src/components/popups/entrance/template.ts
@@ -0,0 +1,27 @@
+const templatePopupEntrance: string = `
+   <div class="popup entrance-popup">
+    <div class="popup__wrapper">
+      <div class="popup__image">
+      <img class="popup__dog" src="assets/images/popups/popup-entrance.svg" alt="Entrance">
+      </div>
+      <div class="popup__content">
+        <div class="popup__header">
+          <h3 class="popup__header_heading">Добро пожаловать!</h3>
+          <p class="popup__header_text">Рислинг очень рад вас видеть! Войдите, чтобы видеть свой прогресс и сохранённые слова</p>
+        </div>
+        <form class="popup__inputs">
+          <input class="popup__input" type="email" placeholder="Email">
+          <input class="popup__input" type="password" placeholder="Пароль">
+        </form>
+        <div class="popup__footer">
+          <button class="button button-additional popup__button button-entrance">Войти</button>
+          <div class="popup__proposal">
+            <span class="popup__proposal_text">Еще нет аккаунта?</span>
+            <button class="button-transparent popup__proposal_button" data-popup="registration">Регистрация</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+export default templatePopupEntrance;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import './global.scss';
 import { addHeader } from './components/header/header';
 import { addMain } from './components/pages/main-page/main-page';
 import { addFooter } from './components/footer/footer';
+import { renderPopupOverlay } from './components/popups/popups';
 
 addHeader();
 addMain();
 addFooter();
+renderPopupOverlay();
