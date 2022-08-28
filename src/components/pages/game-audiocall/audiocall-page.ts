a3395809af500c5f39ddda2b56d95ffc93e42519
Maksym Lytvyn <makshow.at@gmail.com>
2022-08-28 18:29:37 +0300

    feat: game start screen responsive

--- src/components/pages/game-audiocall/audiocall-page.ts
+++ src/components/pages/game-audiocall/audiocall-page.ts
@@ -13,6 +13,7 @@ import { SERVER } from '../../../controllers/loader';
 import {
   addUsersRightWordFromAudiocall, addUsersWrongWordFromAudiocall,
 } from '../../../controllers/api-services/games';
+import { randomizerWord } from '../game-common/game-common';
 
 const groupNumber: number = getGroupNumber();
 
@@ -23,14 +24,6 @@ let allGroupWords: Words;
 
 const rightWords: Words = [];
 const wrongWords: Words = [];
-
-const randomizerWord = (wordsSet: Set<Word>) => {
-  const answer: Word = Array.from(wordsSet)[Math.floor(Math.random() * wordsSet.size)];
-
-  wordsSet.delete(answer);
-
-  return answer;
-};
 
 const randomizerWords = (array: Words, answer: Word) => {
   const wordsSet: Set<Word> = new Set([answer]);
--- src/components/pages/game-audiocall/template.ts
+++ src/components/pages/game-audiocall/template.ts
@@ -13,7 +13,7 @@ const templateAudiocallWindow: string = `
     <div class="game-window__text-content">
       <h4 class="game-window__heading">Аудиовызов</h4>
       <p class="game-window__text game-window__text_description">Прослушайте слово и выберите правильный вариант из пяти предложенных</p>
-      <p class="game-window__note">Для управления с клавиатуры используйте клавиши “1”–“5”, “Пробел” — пропустить.</p>
+      <p class="game-window__note">Для управления с клавиатуры используйте клавиши&nbsp;“1”–“5”, “Пробел”&nbsp;—&nbsp;пропустить.</p>
     </div>
     
     <div class="game-window__categories">
